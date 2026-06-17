c883faacf9bbc44a527408ba7b7f744fb1f27500
Tefi06 <estefcarbe1707@gmail.com>
2026-06-17 15:20:16 -0600

    Crear CategoriasController
    Se crea el controlador categorías con el endpoint Get básico.

---
+++ Backend/MysticBox.Api/Controllers/CategoriasController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace MysticBox.Api.Controllers
+
+{ 
+    [ApiController]
+    [Route("api/categorias")]
+
+    public class CategoriasController : ControllerBase 
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Ver categorias");
+        }
+    }
+}
