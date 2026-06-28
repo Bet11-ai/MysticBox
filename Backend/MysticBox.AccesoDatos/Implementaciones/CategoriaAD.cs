71110b1768cc6a047dd39300f9d2d530c1afb6dc
Tefi06 <estefcarbe1707@gmail.com>
2026-06-27 20:14:32 -0600

    Implementación del Crud de categorías
    Se implementa el controlador, lógica y acceso a la base de categorías sin la clase unidad de trabajo

---
+++ Backend/MysticBox.AccesoDatos/Implementaciones/CategoriaAD.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MysticBox.Dominio.Entidades;
+using MysticBox.Dominio.InterfacesAD;
+using Microsoft.EntityFrameworkCore;
+using MysticBox.AccesoDatos.Contexto;
+
+//using System.Data.SqlClient;
+//using System.Linq;
+//using System.Text;
+
+
+namespace MysticBox.AccesoDatos.Implementaciones
+{
+    public class CategoriaAD : ICategoriaAD
+    {
+        private readonly MysticBoxContext _context;
+
+        public CategoriaAD(MysticBoxContext context)
+        {
+            _context = context;
+        }
+
+        //Obtener categorias
+        public async Task<List<Categoria>> ObtenerCategorias()
+        {
+            return await _context.Categorias.ToListAsync();
+        }
+
+        //Obtener por Id
+        public async Task<Categoria?> ObtenerCategoriaPorId(int idCategoria)
+        {
+            return await _context.Categorias
+                .FirstOrDefaultAsync(c => c.IdCategoria == idCategoria);
+        }
+
+
+        //Crear Categorias
+        public async Task<Categoria> CrearCategoria(Categoria categoria)
+        {
+            _context.Categorias.Add(categoria);
+            await _context.SaveChangesAsync();
+
+            return categoria;
+        }
+
+
+        //Actualiza Categorias
+        public async Task<bool> ActualizarCategoria(int idCategoria, Categoria categoria)
+        {
+            var existente = await _context.Categorias.FindAsync(idCategoria);
+
+            if (existente == null) 
+                
+                    return false;
+
+            existente.NombreCategoria = categoria.NombreCategoria;
+            existente.Descripcion = categoria.Descripcion;
+
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+        //Elimina categorias
+        public async Task<bool> EliminarCategoria(int idCategoria)
+        {
+            var categoria = await _context.Categorias.FindAsync(idCategoria);
+
+            if (categoria == null) return false;
+
+            _context.Categorias.Remove(categoria);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
--- Backend/MysticBox.Api/Program.cs
+++ Backend/MysticBox.Api/Program.cs
@@ -1,3 +1,10 @@
+using MysticBox.AccesoDatos.Implementaciones;
+using MysticBox.Dominio.InterfacesAD;
+using MysticBox.Dominio.InterfacesLN;
+using MysticBox.LogicaNegocio.Implementaciones;
+using Microsoft.EntityFrameworkCore;
+using MysticBox.AccesoDatos.Contexto;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
@@ -7,6 +14,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//builder.Services.AddScoped<UNIDAD>
+builder.Services.AddScoped<ICategoriaLN, CategoriaLN>();
+builder.Services.AddScoped<ICategoriaAD, CategoriaAD>();
+
+
 
 var app = builder.Build();
 
